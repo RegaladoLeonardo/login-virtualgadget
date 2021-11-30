9e25c744146b68e289f53074f56c1861ab986652
Leonardo <regalado.zuniga.leonardo.daniel@gmail.com>
2021-11-30 03:36:12 -0600

    funciona login alumno

--- api/routes/rutas.js
+++ api/routes/rutas.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const mysqlConection = require('../conection/conection');
 const bcrypt = require('bcrypt');
-
+const saltRounds = 10;
 
 const secretKey = '9070N_C0_see?#';
 
@@ -54,7 +54,8 @@ router.get('/verifyToken',(req,res)=>{
 
 
 router.post('/signup',(req, res)=>{
-    const { username, password, name, apellidop, apellidom, fechaNac, valor } = req.body;
+    let { username, password, name, apellidop, apellidom, fechaNac, valor } = req.body;
+    password = bcrypt.hashSync( password , saltRounds);
     const Usu = req.body;
     mysqlConection.query(`insert into usuarios(username, password, apm, app, fechadenacimiento, valor, nombre) values( '${username}'    ,'${password}','${apellidom}','${apellidop}','${fechaNac}',${valor},'${name}' )`,
     (err,rows,fields)=>{
@@ -72,31 +73,68 @@ router.post('/signup',(req, res)=>{
 
     })
 }) 
+/*
+bcrypt . compare ( myPlaintextPassword ,  hash ,  function ( error ,  resultado )  { 
+    // result == true 
+} ) ; 
+*/
 
 router.post('/signin',(req, res)=>{
     const { username, password } = req.body;
-    mysqlConection.query(`SELECT * FROM usuarios WHERE username='${username}' and password='${password}'`,
+    mysqlConection.query(`SELECT * FROM usuarios WHERE username='${username}' `,//and password='${password}
     (err,rows,fields)=>{
 
         if(!err){
             let message  ='';
-            console.log(rows);
+            console.log('-----------------------------');
+            console.log(JSON.parse(JSON.stringify(rows[0])));
+            console.log('-----------------------------');
             let resultadoL = {message, rows}
+        
+            let alumno = JSON.parse(JSON.stringify(rows[0]));
+
+            //let alumno = Object.keys(rows)
+            //console.log(Object.values(rows));
+
+            console.log('Estoy comparando para=> '+password+' y ' + alumno.password  );
+            bcrypt.compare(password , alumno.password , function( error ,  resultado ){
+            //console.log(JSON.parse(JSON.stringify(resultadoT)));
+                
+                if(resultado){
+                    let tokenReadyL = generateToken(rows);
+                    console.log('Este es el token de login: ' + tokenReadyL);
+                    message  ='Se encontro al usuario exitosamente!';
+                    console.log('existe el usuario');
+                    const resultadoL = {message, rows, tokenReadyL}
+                    res.status(200).json(resultadoL);
+
+                }else{
+
+                    message  ='No existe el usuario';
+                    console.log('No existe el usuario');
+                    
+                    res.status(404).json(resultadoL);
+                    console.log('EL ERROR: '+ err) 
+                }
+            });
+            /*
             if(rows.length==0){
+                
                 message  ='No existe el usuario';
                 console.log('No existe el usuario');
                 
                 res.status(404).json(resultadoL);
             }else{
+                let tokenReadyL = generateToken(rows);
+                console.log('Este es el token de login: ' + tokenReadyL);
                 message  ='Se encontro al usuario exitosamente!';
                 console.log('existe el usuario');
-                const resultadoL = {message, rows}
+                const resultadoL = {message, rows, tokenReadyL}
                 res.status(200).json(resultadoL);
-            }
-        }else{
-            console.log(err);
-        }
-
+                */
+            }else{
+                console.log(err);
+                }
     })
 }) 
 
