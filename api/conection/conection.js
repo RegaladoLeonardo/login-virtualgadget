5021e86dee5cee68674e0c4f8c7c63c816a4edca
Leonardo <regalado.zuniga.leonardo.daniel@gmail.com>
2021-11-30 05:10:19 -0600

    todo god

--- api/conection/conection.js
+++ api/conection/conection.js
@@ -9,6 +9,15 @@ const mysqlConection = mysql.createConnection({
 })
 
 
+/*
+const mysqlConection = mysql.createConnection({
+    host:'us-cdbr-east-04.cleardb.com', 
+    user: 'b3536c0cd563b4', 
+    password:'a038bf91',
+    database:'heroku_a5616534128b5ae' ,
+    port: ''
+})
+*/
 mysqlConection.connect(err=>{
 
     if(err){
